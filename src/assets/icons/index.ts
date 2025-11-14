009970fb8046e1111fac1a9134be5612d1a9d02c
suji <suji@sujiui-MacBookAir.local>
2025-11-14 14:47:27 -0500

    feat: icon img

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as UserIcon } from "@/assets/icons/icon_user.svg?react";
+export { default as CartIcon } from "@/assets/icons/icon_cart.svg?react";
+export { default as SearchIcon } from "@/assets/icons/icon_search.svg?react";
+export { default as WishIcon } from "@/assets/icons/icon_wish.svg?react";
